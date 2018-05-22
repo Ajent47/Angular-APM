4d1985041f96820ee99b436e8a98529bc473382f
AJENT47 <ajnt47@gmail.com>
2018-05-22 16:51:27 +0530

    Lifecycle hooks, custom pipes, filtering list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from './product';
 
 @Component({
@@ -7,12 +7,23 @@ import { IProduct } from './product';
   styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string;
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
@@ -22,8 +33,8 @@ export class ProductListComponent {
     "price": 19.95,
     "starRating": 3.2,
     "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
+  },
+  {
     "productId": 2,
     "productName": "Garden Cart",
     "productCode": "GDN-0023",
@@ -32,9 +43,22 @@ export class ProductListComponent {
     "price": 32.99,
     "starRating": 4.2,
     "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+  }];
 
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 }

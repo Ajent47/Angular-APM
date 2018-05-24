afad4b49c4f159b072b33066fc6c5d6cf7d8f88f
AJENT47 <ajnt47@gmail.com>
2018-05-24 17:11:25 +0530

    Refractoring I

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { SharedModule } from './../shared/shared.module';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductGuardService } from './product-guard.service';
+import { ProductListComponent } from './product-list.component';
+import { ProductService } from './product.service';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ ProductGuardService ],
+        component: ProductDetailComponent
+      }
+    ]),
+    SharedModule
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
